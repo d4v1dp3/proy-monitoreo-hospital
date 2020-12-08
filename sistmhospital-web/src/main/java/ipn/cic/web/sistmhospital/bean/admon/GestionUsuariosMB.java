98674078e5838489f51c9c870f9c3354ce482800
Jose <leojjosepf@gmail.com>
2020-12-07 18:38:41 -0600

    Mod pantallas 1

--- sistmhospital-web/src/main/java/ipn/cic/web/sistmhospital/bean/admon/GestionUsuariosMB.java
+++ sistmhospital-web/src/main/java/ipn/cic/web/sistmhospital/bean/admon/GestionUsuariosMB.java
@@ -72,8 +72,8 @@ public class GestionUsuariosMB implements Serializable {
     public void altaMedico(){
         Map<String, Object> options = new HashMap<String, Object>();
         options.put("modal", true);
-        options.put("width", 640);
-        options.put("height", 340);
+        options.put("width", 750);
+        options.put("height", 640);
         options.put("contentWidth", "100%");
         options.put("contentHeight", "100%");
         options.put("headerElement", "customheader");
@@ -89,8 +89,8 @@ public class GestionUsuariosMB implements Serializable {
     public void altaPaciente(){
         Map<String, Object> options = new HashMap<String, Object>();
         options.put("modal", true);
-        options.put("width", 640);
-        options.put("height", 340);
+        options.put("width", 700);
+        options.put("height", 640);
         options.put("contentWidth", "100%");
         options.put("contentHeight", "100%");
         options.put("headerElement", "customheader");
